00b93bdefb91423698f435283aa201411e2138d7
Soham Sanjay Pawar <sohampawar1866@gmail.com>
2025-05-27 18:16:29 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'E-votrix',
+  description: 'Created By Soham Pawar',
 }
 
 export default function RootLayout({
