88c40656a713826a7b06540856f64e44da558510
Soham Sanjay Pawar <sohampawar1866@gmail.com>
2025-05-27 22:41:53 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'E-Votrix',
+  title: 'E-Votrix: Secure Blockchain Voting on Ethereum Sepolia',
   description: 'Created By Soham Pawar',
 }
 
